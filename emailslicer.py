468b698ae97ca6d3cc80ad471a285ee286a00bed
Angeluche <ucheorji9974@gmail.com>
2022-10-05 02:04:06 +0000

    email slicer project

--- emailslicer.py
+++ emailslicer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """This project is to mimic the emial slicer where an email is being reduced
 to names and domain.
 1. it asks the user for their names
@@ -7,14 +6,13 @@ to names and domain.
  3. use rindex tpo get the location of  a given character
  finally it  uses string slicing to slice of the given character"""
 
-choice = 1
 while True:
     user_email =str(input("Please put your valid email: ")).strip()
-    if "@" not in user_email:
+    index1 = user_email.split('@')
+    name = index1[0]
+    domain = index1[-1]
+    if "@" not in user_email or ' ' in name or '.' not in domain or " " in domain:
         print('please put in a valid email address')
     else:
-        index1 = user_email.rindex('@')
-        name = user_email[:index1].title()
-        domain = user_email[index1 + 1:].title()
         print("Your Name: {0}\nYour Doamin: {1}".format(name, domain))
         break
